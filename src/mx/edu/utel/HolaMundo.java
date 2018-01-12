07456681d34c50988f0c7d8bded31bf81e3bbaac
atellezf <alejandro.tellez@gmail.com>
2018-01-11 21:37:40 -0600

    Código de Conceptos básicos de la POO

---
+++ src/mx/edu/utel/HolaMundo.java
@@ -0,0 +1,28 @@
+/**
+ * Declaración del paquete al que pertenece la clase
+ */
+package mx.edu.utel;
+
+// Lista de paquetes a utilizar en la clase
+// import java.util.Scanner;
+
+/**
+ * Clase de prueba HolaMundo
+ * @author alejandro
+ */
+public class HolaMundo {
+    
+    /**
+     * Declaración de atributos
+     */
+    private int valor;
+    
+    /**
+     * Declaración de métodos
+     * @param args Argumentos del método
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+    }
+    
+}
